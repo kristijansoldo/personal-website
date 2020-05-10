1e9220d7ac6663a38eb85cdb4a85d152b2d23931
kristijansoldo <soldokristijan@yahoo.com>
2020-05-10 22:11:00 +0200

    Project structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import Hero from "../components/hero/hero";
 
 export default function Home() {
     return(
-        <div>Hello world!</div>
+        <div>
+            <Navigation />
+            <Hero />
+        </div>
     );
 }
