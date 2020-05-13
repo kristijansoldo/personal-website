55b54ac760c6518a82a3759896aa974ad8c5f0de
kristijansoldo <soldokristijan@yahoo.com>
2020-05-13 18:36:29 +0200

    Added metadata

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -4,6 +4,7 @@ import React from "react"
 // Import components
 import Navigation from "../navigation/navigation";
 import Footer from "../footer/footer";
+import { Helmet } from "react-helmet"
 
 /**
  * Layout of website.
@@ -15,6 +16,10 @@ import Footer from "../footer/footer";
 export default function Layout({ children }) {
   return (
     <main>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Kristijan Soldo | Web Developer from Croatia</title>
+        </Helmet>
       <Navigation />
       {children}
       <Footer />
