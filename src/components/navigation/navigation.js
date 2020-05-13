094e32b6c25cf78b4362c9fb933343de413e9960
kristijansoldo <soldokristijan@yahoo.com>
2020-05-13 17:30:50 +0200

    Added .idea to .gitignore

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,16 +1,20 @@
 import React from "react"
-import Section from "../../common/section/section"
 import LogoSvg from "../../../static/logo.svg"
 
 import styles from "./navigation.module.scss"
 
+/**
+ * Navigation component.
+ *
+ * @returns {*}
+ * @constructor
+ */
 export default function Navigation() {
   return (
-    <Section className={styles.navigation}>
+    <nav className={styles.navigation}>
       <div className={styles.logo}>
         <LogoSvg />
       </div>
-      Navigation
-    </Section>
+    </nav>
   )
 }
