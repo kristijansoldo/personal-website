e9f31e5dcbc9a64f27bdd707fa44243654d723c5
kristijansoldo <soldokristijan@yahoo.com>
2020-05-21 18:12:45 +0200

    Added tech stack section

---
+++ src/components/techstack/techstack.js
@@ -0,0 +1,22 @@
+// React import
+import React from "react"
+
+// Import components
+import Section from "../common/section/section";
+
+// Import styles
+import styles from "./techstack.module.scss"
+
+/**
+ * Tech stack component.
+ *
+ * @returns {*}
+ * @constructor
+ */
+export default function TechStack() {
+    return (
+        <Section className={styles.techStack}>
+
+        </Section>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,20 @@ import React from "react"
 import Hero from "../components/hero/hero";
 import Services from "../components/services/services";
 import Layout from "../components/layout/layout";
+import TechStack from "../components/techstack/techstack";
 
+/**
+ * Home page.
+ *
+ * @returns {*}
+ * @constructor
+ */
 export default function Home() {
     return (
         <Layout>
             <Hero/>
             <Services/>
+            <TechStack/>
         </Layout>
     );
 }
